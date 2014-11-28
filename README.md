3981271075df87a3b2c48d95ae49375bfc5fa5d6
ntdaley <ntdaley@ntdaley.org>
2014-11-28 00:46:11 +0000

    Initial commit - gulp plugin for stripping angular logging

---
+++ README.md
@@ -0,0 +1,28 @@
+gulp-strip-ng-log
+=================
+
+Gulp plugin to strip calls to AngularJS's $log functions.
+
+Inspired by [gulp-strip-debug](https://github.com/sindresorhus/gulp-strip-debug)
+
+Install
+=======
+```npm install --save gulp-strip-ng-log```
+
+Usage
+=====
+```
+var gulp = require('gulp');
+var stripNgLog = require('gulp-strip-ng-log');
+
+gulp.task('stripLogging', function() {
+    return gulp.src('app/app.js')
+        .pipe(stripNgLog())
+        .pipe(gulp.dest('build'));
+});
+```
+
+License
+=======
+Apache 2.0 License
+&copy; Nicholas Daley
---
+++ index.js
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2014 Nicholas Daley
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+var gutil = require('gulp-util');
+var through = require('through2');
+var stripLog = require('strip-ng-log');
+
+module.exports = function() {
+    return through.obj(function(file, encoding, done) {
+        if(file.isNull()) {
+            done(null, file);
+            return;
+        } else if( file.isStream() ) {
+            done(new gutil.PluginError('gulp-strip-ng-log', 'Streaming not supported'));
+            return;
+        } else {
+            try {
+                file.contents = new Buffer(stripLog(file.contents.toString()).toString());
+            } catch(err) {
+                this.emit('error', new gutil.PluginError('gulp-strip-ng-log', err, {fileName : file.path}));
+            }
+        }
+    });
+};
